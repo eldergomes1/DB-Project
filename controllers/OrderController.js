bda6d8b0f818146903b78eac470ba390d6ddfb9c
Elder Gomes <eldergomesmalave@gmail.com>
2021-06-26 23:02:47 +0200

    ya se puede donar cuando no hay productos a la venta

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -5,19 +5,22 @@ const index = async function(req, res){
     let idCampaign = req.query.id1
     let idusuario = req.query.id2
     try {
-        if (idCampaign !== undefined && idusuario !== undefined) {
-            // res.send("id1="+idCampaign+" id2="+idusuario);
-            let response = await db.query('select c.nombre, p.idproducto, p.titulo, p.costo, p.tipo from campana as c join equipo as e on e.idcampana=c.idcampana join producto as p on p.idcampana=c.idcampana where c.idcampana=$1 and e.idusuario=$2',
-            [idCampaign, idusuario]);
-
-            if (response.rowCount == 0) {
-                throw "sin registros";
-            }
-
-            res.render('order/create', {data:response.rows, query:{id1:idCampaign, id2:idusuario}});
-        }else{
+        if (idCampaign === undefined || idusuario === undefined) {
             throw "parametros insuficientes";
         }
+        // comprobamos que el usuario este asociado a la campaña
+        const campanaQuery = "select c.nombre from campana as c join equipo as e on e.idcampana=c.idcampana where c.idcampana=$1 and e.idusuario=$2"
+        let {rows:[campana]} = await db.query(campanaQuery, [idCampaign, idusuario]);
+        if (campana == undefined) {
+            throw "no asociados";
+        }
+
+        const productsQuery = "select * from producto where idcampana=$1"
+        let {rows:productos = []} = await db.query(productsQuery, [idCampaign]);
+
+        // res.render('order/create', {data:response.rows, query:{id1:idCampaign, id2:idusuario}});
+        res.render('order/create', {campana, productos, query:{id1:idCampaign, id2:idusuario}});
+        
     } catch (error) {
         res.send('<h1>ocurrio un error</h1>');
     }
@@ -27,53 +30,57 @@ const donar = async function(req, res){
     let idCampaign = req.query.id1
     let idusuario = req.query.id2
     try {
-        if (idCampaign !== undefined && idusuario !== undefined) {
-            let {rowCount, rows:Campaign} = await db.query('select c.recaudado from equipo as e join campana as c on c.idcampana = e.idcampana where e.idcampana=$1 and e.idusuario=$2', [idCampaign, idusuario]);
-            if (rowCount == 0) {
-                throw 'usuario y campana no relacionados';
-            }
-            let productSelected = req.body.products.filter(produt => produt.isselected === 'on');
-            if (productSelected.length == 0 && req.body.donacion === '') {
-                throw 'productos y donacion no proporcionados';
-            }
-            const client = await db.connect();
-            try {
-                await client.query('BEGIN');
-                const insertText = 'insert into orden(idcampana, idusuario) values ($1, $2) returning idorden';
-                let resOrden = await client.query(insertText, [idCampaign, idusuario]);
-                let subTotal = 0;
-                for (let i = 0; i < productSelected.length; i++) {
-                    let element = productSelected[i];
-                    let productQuery = 'select * from producto as p where p.idproducto=$1 and p.idcampana=$2';
-                    let {rowCount:productNumber, rows:products} = await db.query(productQuery, [element.idproducto, idCampaign]);
-                    if (productNumber > 0) {
-                        let product = products[0];
-                        subTotal += Number(product.costo);
-                        await client.query('insert into venta(idorden, unidades, titulo, costo) values ($1, 0, $2, $3)', [resOrden.rows[0].idorden, product.titulo, product.costo]);
-                    }else{
-                        throw 'producto no encontrado';
-                    }
-                }
-                if (req.body.donacion != '') {
-                    await client.query('insert into donacion(idorden, monto) values ($1, $2)', [resOrden.rows[0].idorden, req.body.donacion]);
-                }
-
-                let total = subTotal + (req.body.donacion == '' ? 0 :Number(req.body.donacion));
-                if (total > 0) {
-                    let query = "update campana set recaudado=$1 where idcampana=$2";
-                    await client.query(query, [Number(Campaign[0].recaudado) + total, idCampaign]);
-                }
-
-                await client.query('COMMIT');
-                res.send('codigo de orden: '+resOrden.rows[0].idorden);
-            } catch (error) {
-                await client.query('ROLLBACK');
-                throw error;
-            }finally{
-                client.release();
-            }
-        }else{
+        if (idCampaign === undefined || idusuario === undefined) {
             throw "parametros insuficientes";
+        }
+
+        let {rowCount, rows:Campaign} = await db.query('select c.recaudado from equipo as e join campana as c on c.idcampana = e.idcampana where e.idcampana=$1 and e.idusuario=$2', [idCampaign, idusuario]);
+        if (rowCount == 0) {
+            throw 'usuario y campana no relacionados';
+        }
+        if (req.body.products === undefined && req.body.donacion === '') {
+            throw 'productos y donacion no proporcionados';
+        }
+        let productSelected = (req.body.products ?? []).filter(produt => produt.isselected === 'on');
+        // if (productSelected.length == 0 && req.body.donacion === '') {
+        //     throw 'productos y donacion no proporcionados';
+        // }
+        const client = await db.connect();
+        await client.query('BEGIN');
+        try {
+            const insertText = 'insert into orden(idcampana, idusuario) values ($1, $2) returning idorden';
+            let resOrden = await client.query(insertText, [idCampaign, idusuario]);
+            let subTotal = 0;
+            for (let i = 0; i < productSelected.length; i++) {
+                let element = productSelected[i];
+                let productQuery = 'select * from producto as p where p.idproducto=$1 and p.idcampana=$2';
+                let {rowCount:productNumber, rows:products} = await db.query(productQuery, [element.idproducto, idCampaign]);
+                if (productNumber > 0) {
+                    let product = products[0];
+                    subTotal += Number(product.costo);
+                    await client.query('insert into venta(idorden, unidades, titulo, costo) values ($1, 0, $2, $3)', [resOrden.rows[0].idorden, product.titulo, product.costo]);
+                }else{
+                    throw 'producto no encontrado';
+                }
+            }
+            if (req.body.donacion != '') {
+                await client.query('insert into donacion(idorden, monto) values ($1, $2)', [resOrden.rows[0].idorden, req.body.donacion]);
+            }
+
+            let total = subTotal + (req.body.donacion == '' ? 0 :Number(req.body.donacion));
+            if (total > 0) {
+                let query = "update campana set recaudado=$1 where idcampana=$2";
+                await client.query(query, [Number(Campaign[0].recaudado) + total, idCampaign]);
+            }
+
+            await client.query('COMMIT');
+            res.send('codigo de orden: '+resOrden.rows[0].idorden);
+        } catch (error) {
+            console.log(error);
+            await client.query('ROLLBACK');
+            throw error;
+        }finally{
+            client.release();
         }
     } catch (error) {
         console.log(error);
